63eb30afd0f354d3429604fecac3abec2554dd95
vera <vera.filippova.79@mail.ru>
2024-03-21 15:05:51 +0300

    Operator added

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int summa1, bonus;
+        summa1 = 100;
+        int summaPlus = 1100;
+        bonus = 1;
+        if (summaPlus > 1000) {
+
+            bonus = summaPlus / 100;
+
+            summa1 = summa1 + summaPlus + bonus;
+
+        } else {
+            summa1 = summa1 + summaPlus;
+        }
+        System.out.println("Итоговая сумма: " + summa1);
+
+    }
+}
+
